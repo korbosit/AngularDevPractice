3bd7ab1ce5afa5088d4b0c3e4f7220249bf7e429
Vlaimir Korba <krissky1986@gmail.com>
2024-02-07 10:50:07 +0200

    Conditionals if else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,11 @@ import { RouterOutlet } from '@angular/router';
   template: `
     @if (isAdmin) {
     <button>Erase database</button>
+    } @else {
+    <p>You are not authorized.</p>
     }
   `,
 })
 export class AppComponent {
-  isAdmin = true;
+  isAdmin = false;
 }
